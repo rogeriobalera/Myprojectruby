621cbebaacf78018df26b7156d675cc0fde7cc36
rogeriobalera <profissionalrogerio01@gmail.com>
2021-11-01 20:38:28 -0300

    PRojetoRuby

---
+++ Programa1.rb
@@ -0,0 +1,9 @@
+print "Digite o seu nome: "
+
+name = gets.chomp
+
+print "Digite o seu sobrenome: "
+
+sobrenome = gets.chomp
+
+puts "Hello #{name} #{sobrenome}!"
---
+++ Programe1.rb
@@ -0,0 +1,29 @@
+puts "--------------------------------------"
+            puts         "WELCOME TO PROGRAM"
+puts "--------------------------------------"
+
+print "Type a number entire : "
+
+number1 = gets.chomp.to_i
+
+print "Type a second number entire : "
+
+number2 = gets.chomp.to_i
+
+addition = number1 + number2    
+
+subtraction = number1 - number2
+
+division = number1 / number2
+
+multiplication = number1 * number2
+
+puts "the addition between the two is #{addition}"
+puts "and subtraction is #{subtraction}" 
+puts "the divisioh is #{division}" 
+puts "and multiplication is #{multiplication}"
+
+puts "--------------------------------------"
+   puts  "THANK FOR USING MY PROGRAM"
+puts "--------------------------------------"
+
